ffa58ee6dc438a528698221b1eef335632dfb944
zhangyuge <398698424@qq.com>
2024-05-27 19:41:59 +0800

    修改mybatis-plus分页插件配置

--- newbie-common/src/main/java/com/newbie/common/config/MybatisPlusConfig.java
+++ newbie-common/src/main/java/com/newbie/common/config/MybatisPlusConfig.java
@@ -3,9 +3,15 @@ package com.newbie.common.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import jakarta.annotation.Resource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,15 +24,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.newbie.**.mapper")
 public class MybatisPlusConfig {
+    @Resource
+    private DataSource dataSource;
     /**
      * 分页插件
      * @return
      */
     @Bean
     MybatisPlusInterceptor mybatisPlusInterceptor(){
-        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        return mybatisPlusInterceptor;
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        try {
+            Connection connection = dataSource.getConnection();
+            DatabaseMetaData metaData = connection.getMetaData();
+            String url = metaData.getURL();
+            if (url.contains("mysql")) {
+                paginationInnerInterceptor.setDbType(DbType.MYSQL);
+            }else if (url.contains("oracle")) {
+                paginationInnerInterceptor.setDbType(DbType.ORACLE);
+            }else if (url.contains("postgresql")) {
+                paginationInnerInterceptor.setDbType(DbType.POSTGRE_SQL);
+            }
+            MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+            mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+            return mybatisPlusInterceptor;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
